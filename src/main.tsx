7f47e2d862b0d0e524f6a663dc95eb2b4b6c20c4
mpcodingpl <97036774+mpcodingpl@users.noreply.github.com>
2024-02-23 19:59:48 +0100

    Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,26 @@ import { Provider } from 'react-redux'
 
 import { createRoot } from 'react-dom/client'
 
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: process.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', 'vercel.app'],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
+
 const container = document.getElementById('root')
 
 if (!container) throw new Error("Could not find root element with id 'root'")
